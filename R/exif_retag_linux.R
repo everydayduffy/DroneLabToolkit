5e6d1f8f46c28a860917cc684a715104e175ce1f
everydayduffy <james.philip.duffy@gmail.com>
2018-03-23 10:12:24 +0000

    applied better naming practice to variables

--- R/exif_retag_linux.R
+++ R/exif_retag_linux.R
@@ -1,74 +1,82 @@
-#' Retag photos with GPS info from Ardupilot dataflash logs
+#' Retag images with GPS info from Ardupilot dataflash logs
 #'
-#' @param timediff The difference in time between camera and GPS
-#' @param photo.folder.path The location photos to be tagged
-#' @param log.file.path The full path of the associated dataflash log
-#' @param proj.name A name for this project (appended to output .csv)
-#' @param csv.out Output folder for .csv file
-#' @param leapsecs Number leapseconds in GPS time since the epoch. default
+#' @param time_diff The difference in time between camera and GPS
+#' @param img_path The location photos to be tagged
+#' @param log_file The full path of the associated dataflash log
+#' @param proj_name A name for this project (appended to output .csv)
+#' @param csv_out Output folder for .csv file
+#' @param leap_secs Number leapseconds in GPS time since the epoch. default
 #' is 18
 #' @author James P. Duffy
 #' @export
 
 exif_retag_linux <-
   # arguments for exif retag
-  function(timediff=0,photo.folder.path,log.file.path,proj.name,csv.out,
-           leapsecs= seq(10:50)) {
-    ##Leapsecs to default at 18 (as of December 2016)
-    if(missing(leapsecs)){leapsecs = 18}
+  function(time_diff=0,img_path,log_file,proj_name,csv_out,
+           leap_secs= seq(10:50)) {
+
+    ##Number of seconds in a week
+    secs_week <- 604800
+    ##GPS epoch date
+    epoch_date <- "1980-01-06 00:00:00"
+
+    ##leap_secs to default at 18 (as of December 2016)
+    if(missing(leap_secs)){leap_secs = 18}
     ##Part 1: Obtain exif times
-    exif.time.data <- as.data.frame(system(paste0("exiftool -T -filename -createdate ",
-                                                  photo.folder.path), inter=TRUE))
-    colnames(exif.time.data) <- "name_time_date"
+    exif_times <- as.data.frame(system(paste0("exiftool -T -filename -createdate ",
+                                                  img_path), inter=TRUE))
+    colnames(exif_times) <- "name_time_date"
     ##Tidy the data into separate columns
-    exif.time.data <- tidyr::separate(exif.time.data, name_time_date, into =  c("name", "time"), sep ="\t") %>%
+    exif_times <- tidyr::separate(exif_times, name_time_date,
+                                  into =  c("name", "time"), sep ="\t") %>%
       tidyr::separate(.,time, into =  c("date", "time"), sep =" ") %>%
       data.frame()
-    ##Part 2: Match up photo times with log data
-    ##Number of seconds in a week
-    secsweek <- 604800
-    ##GPS epoch date
-    epochdate <- "1980-01-06 00:00:00"
+
+    ##############################################
+    ##Part 2: Match up photo times with log data##
+    ##############################################
+
     ##Read in bin log that has been converted to .log file
-    log.data <- read.delim(log.file.path, h=F, sep= ",",stringsAsFactors=FALSE)
+    log_data <- read.delim(log_file, h=F, sep= ",",stringsAsFactors=FALSE)
     ##Filter out GPS data
-    log.data <- dplyr::filter(log.data, V1=="GPS")
+    log_data <- dplyr::filter(log_data, V1=="GPS")
     ##Check the style of log (i.e. how many columns)
-    if (ncol(log.data)<19) {
+    if (ncol(log_data)<19) {
       add.val <- 0
-    } else if (ncol(log.data)==19) {
+    } else if (ncol(log_data)==19) {
       add.val <- 1
     }
     ##Filter out relevant columns
-    log.data <- dplyr::select(log.data, c((2+add.val):(4+add.val),(7+add.val):(9+add.val)))
+    log_data <- dplyr::select(log_data, c((2+add.val):(4+add.val),(7+add.val):(9+add.val)))
     ##Add column names
-    colnames(log.data) <- c("status","timeMS","weeks","lat","long","alt")
+    colnames(log_data) <- c("status","timeMS","weeks","lat","long","alt")
     ##Convert 'weeks' data into numeric values
-    log.data$weeks <- as.numeric(as.character(log.data$weeks))
+    log_data <- log_data %>%
+      dplyr::mutate(.,weeks = as.numeric(as.character(weeks)))
     ##Create a list of GPS 'epoch' dates on which to add GPS times to
-    epoch <- data.frame(time=c(rep(epochdate,dim(log.data)[1])))
+    epoch <- data.frame(time=c(rep(epoch_date,nrow(log_data))))
     ##Format them correctly so 'seconds' can be added
     epoch$time <- strptime(epoch$time,"%Y-%m-%d %H:%M:%S")
-    ##Add seconds calculated from the data to the 'epoch' times + timediff
-    epoch$time <- epoch$time + (log.data$weeks*secsweek) + (as.numeric(log.data$timeMS)/1000) - leapsecs
+    ##Add seconds calculated from the data to the 'epoch' times + time_diff
+    epoch$time <- epoch$time + (log_data$weeks*secs_week) + (as.numeric(log_data$timeMS)/1000) - leap_secs
     ##Add the time offset from particular dataset (defined at top of script)
-    epoch$newtime <- epoch$time + timediff
+    epoch$newtime <- epoch$time + time_diff
     ##Split date and time into 2 (both old and new)
     epoch <- data.frame(do.call('rbind', strsplit(as.character(epoch$time),' ',fixed=TRUE)),do.call('rbind', strsplit(as.character(epoch$newtime),' ',fixed=TRUE)))
     ##Drop the extra date column (duplicate)
     epoch <- dplyr::select(epoch, -3)
     colnames(epoch) <- c("date","oldtime","time")
     ##Reshuffle data to get relevant information for merging with exif data
-    log.data <- data.frame(epoch$date,epoch$oldtime,epoch$time,log.data$long,log.data$lat,log.data$alt)
-    colnames(log.data) <- c("date","oldtime","time","long","lat","alt")
+    log_data <- data.frame(epoch$date,epoch$oldtime,epoch$time,log_data$long,log_data$lat,log_data$alt)
+    colnames(log_data) <- c("date","oldtime","time","long","lat","alt")
     ##Removes duplicates, just taking the first reading for each second
-    log.data <- subset(log.data, !duplicated(log.data$oldtime))
+    log_data <- subset(log_data, !duplicated(log_data$oldtime))
     ##Remove whitespace
-    log.data[c('long', 'lat', 'alt', 'time')] <- lapply(log.data[c('long', 'lat', 'alt', 'time')], gsub, pattern=" ",replacement="")
+    log_data[c('long', 'lat', 'alt', 'time')] <- lapply(log_data[c('long', 'lat', 'alt', 'time')], gsub, pattern=" ",replacement="")
     ##Tidy up dates and times to make them match
-    log.data$date <- gsub(":", "-", log.data$date)
-    exif.time.data$date <- gsub(":", "-", exif.time.data$date)
-    combo <- merge(x=exif.time.data, y=log.data, by= c("date","time"))
+    log_data$date <- gsub(":", "-", log_data$date)
+    exif_times$date <- gsub(":", "-", exif_times$date)
+    combo <- merge(x=exif_times, y=log_data, by= c("date","time"))
     ##Create a new columns with N/S/E/W references for lat long - to feed into exiftool
     combo$hor <- rep(NA,nrow(combo))
     combo$ver <- rep(NA,nrow(combo))
@@ -87,22 +95,22 @@ exif_retag_linux <-
       }
     }
     ##Combine useful info into a .csv
-    minimal.output <- data.frame(combo$name,combo$time,combo$long,combo$lat,combo$alt,combo$hor,combo$ver)
-    colnames(minimal.output) <- c("name","time","long","lat","alt","hor","ver")
+    min_output <- data.frame(combo$name,combo$time,combo$long,combo$lat,combo$alt,combo$hor,combo$ver)
+    colnames(min_output) <- c("name","time","long","lat","alt","hor","ver")
     ##Write the .csv
-    write.csv(minimal.output, paste0(csv.out,"/",proj.name,"_exif_merge.csv"),row.names=F,quote=FALSE)
+    write.csv(min_output, paste0(csv_out,"/",proj_name,"_exif_merge.csv"),row.names=F,quote=FALSE)
     ##Part 3: Retag the photos
     ##Progress bar
     print("Retag progress")
-    pb <- txtProgressBar(min = 0, max = nrow(minimal.output), style = 3)
+    pb <- txtProgressBar(min = 0, max = nrow(min_output), style = 3)
     ##Loop through all the photos tagging GPS info
-    for (i in 1:nrow(minimal.output))
+    for (i in 1:nrow(min_output))
     {
       system(paste0("exiftool -overwrite_original -F -q -gpslongitude=",
-                    minimal.output$long[i]," -gpslatitude=",minimal.output$lat[i],
-                    " -gpslongituderef=",minimal.output$hor[i]," -gpslatituderef=",
-                    minimal.output$ver[i]," -gpsaltitude=",minimal.output$alt[i],
-                    " -gpsaltituderef=above ",photo.folder.path,"/",minimal.output$name[i]),
+                    min_output$long[i]," -gpslatitude=",min_output$lat[i],
+                    " -gpslongituderef=",min_output$hor[i]," -gpslatituderef=",
+                    min_output$ver[i]," -gpsaltitude=",min_output$alt[i],
+                    " -gpsaltituderef=above ",img_path,"/",min_output$name[i]),
              inter=TRUE)
       setTxtProgressBar(pb, i)
     }
